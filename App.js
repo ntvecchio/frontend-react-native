a41d9dc45bf49a25ec88c6d0d35b14414e76dd4e
Osvaldo Vecchio <osvaldovecchioneto28@gmail.com>
2024-08-01 17:18:35 -0300

    iniciando projeto react native

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>hello world React Native!</Text>
+      <Text>primeiro app para ios e android</Text>
       <StatusBar style="auto" />
     </View>
   );
